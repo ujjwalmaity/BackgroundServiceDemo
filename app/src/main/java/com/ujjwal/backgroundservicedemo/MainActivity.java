d95c90cf292c6c4b7b169c34ea44c2cd663d9036
ujjwal <ujjwalmaity98@gmail.com>
2019-09-07 22:04:53 +0530

    Local Binder i.e IBinder

--- app/src/main/java/com/ujjwal/backgroundservicedemo/MainActivity.java
+++ app/src/main/java/com/ujjwal/backgroundservicedemo/MainActivity.java
@@ -2,14 +2,25 @@ package com.ujjwal.backgroundservicedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+
+    private ServiceConnection serviceConnection;
+    private boolean isServiceBound;
+    private MyService myService;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +34,49 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         intent = new Intent(getApplicationContext(), MyService.class);
+        textView = findViewById(R.id.textView);
     }
 
     public void stopBackgroundService(View view) {
+        stopService(intent);
     }
 
     public void startBackgroundService(View view) {
         startService(intent);
+    }
+
+    public void bindService(View view) {
+        if (serviceConnection == null) {
+            serviceConnection = new ServiceConnection() {
+                @Override
+                public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                    MyService.MyServiceBinder myServiceBinder = (MyService.MyServiceBinder) iBinder;
+                    myService = myServiceBinder.getService();
+                    isServiceBound = true;
+                }
+
+                @Override
+                public void onServiceDisconnected(ComponentName componentName) {
+                    isServiceBound = false;
+                }
+            };
+        }
+
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void unbindService(View view) {
+        if (isServiceBound) {
+            unbindService(serviceConnection);
+            isServiceBound = false;
+        }
+    }
+
+    public void getRandomNumber(View view) {
+        if (isServiceBound) {
+            textView.setText("Random Number: " + myService.getRandomNumber());
+        } else {
+            textView.setText("Service not bound");
+        }
     }
 }
--- app/src/main/java/com/ujjwal/backgroundservicedemo/MyService.java
+++ app/src/main/java/com/ujjwal/backgroundservicedemo/MyService.java
@@ -2,29 +2,91 @@ package com.ujjwal.backgroundservicedemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class MyService extends Service {
+
+    private int randomNumber;
+    private boolean isRandomNumberGeneratorON;
+
+    private final int MIN = 0;
+    private final int MAX = 100;
+
+    class MyServiceBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    private IBinder iBinder = new MyServiceBinder();
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.i("Service Demo", "In onBind");
+        return iBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i("Service Demo", "In onUnBind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.i("Service Demo", "In onReBind");
     }
 
     @Override
     public void onDestroy() {
-        Log.i("Service Demo", "In onDestroy, thread id: " + Thread.currentThread().getId());
+        stopRandomNumberGenerator();
+        Log.i("Service Demo", "Service Destroyed");
         super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("Service Demo", "In onStartCommand, thread id: " + Thread.currentThread().getId());
-        stopSelf(); // Service Cannot be stop from activity, so we stop it from here.
-        return super.onStartCommand(intent, flags, startId);
+
+        isRandomNumberGeneratorON = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startRandomNumberGenerator();
+            }
+        }).start();
+
+//        stopSelf(); // Service Cannot be stop from activity, so we stop it from here.
+        return START_STICKY;
+    }
+
+    private void startRandomNumberGenerator() {
+        while (isRandomNumberGeneratorON) {
+            try {
+                Thread.sleep(1000);
+                if (isRandomNumberGeneratorON) {
+                    randomNumber = new Random().nextInt(MAX) + MIN;
+                    Log.i("Service Demo", "Thread id: " + Thread.currentThread().getId() + "Random Number: " + randomNumber);
+                }
+            } catch (InterruptedException e) {
+                Log.i("Service Demo", "Thread Interrupted");
+            }
+        }
+    }
+
+    private void stopRandomNumberGenerator() {
+        isRandomNumberGeneratorON = false;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
     }
 }

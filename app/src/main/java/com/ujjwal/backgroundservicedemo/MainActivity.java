03d362a3f83eb71e2efcd14124bd8122531811a0
ujjwal <ujjwalmaity98@gmail.com>
2019-09-07 19:35:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/ujjwal/backgroundservicedemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ujjwal.backgroundservicedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void stopBackgroundService(View view) {
+    }
+
+    public void startBackgroundService(View view) {
+    }
+}

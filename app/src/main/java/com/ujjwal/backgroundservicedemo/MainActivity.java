08522b50bb1ea50dcc2801723f27bc8853ccb578
ujjwal <ujjwalmaity98@gmail.com>
2020-03-22 21:10:12 +0530

    Implement Callback

--- app/src/main/java/com/ujjwal/backgroundservicedemo/MainActivity.java
+++ app/src/main/java/com/ujjwal/backgroundservicedemo/MainActivity.java
@@ -7,12 +7,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
 
     private Intent intent, intent2;
 
@@ -27,9 +31,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("Service Demo", "In MainActivity, thread id: " + Thread.currentThread().getId());
+        Log.i(TAG, "In MainActivity, thread id: " + Thread.currentThread().getId());
 
         init();
+
+        bindService();
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bindService();
+            }
+        });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unbindService();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unbindService();
     }
 
     private void init() {
@@ -47,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         startService(intent);
     }
 
-    public void bindService(View view) {
+    public void bindService() {
         if (serviceConnection == null) {
             serviceConnection = new ServiceConnection() {
                 @Override
@@ -65,18 +91,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!isServiceBound) {
+                    try {
+                        Thread.sleep(1000);
+                        getMyService();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
-    public void unbindService(View view) {
+    public void unbindService() {
         if (isServiceBound) {
             unbindService(serviceConnection);
             isServiceBound = false;
         }
     }
 
-    public void getRandomNumber(View view) {
+    public void getMyService() {
         if (isServiceBound) {
-            textView.setText("Random Number: " + myService.getRandomNumber());
+            myService.setCallbacks(new MyService.Callback() {
+                @Override
+                public void getRandomNumber(final int num) {
+                    /*
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText("Random Number: " + num);
+                        }
+                    });
+                    */
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            textView.setText("Random Number: " + num);
+                        }
+                    });
+                }
+            });
         } else {
             textView.setText("Service not bound");
         }
--- app/src/main/java/com/ujjwal/backgroundservicedemo/MyService.java
+++ app/src/main/java/com/ujjwal/backgroundservicedemo/MyService.java
@@ -12,11 +12,23 @@ import java.util.Random;
 
 public class MyService extends Service {
 
+    private static String TAG = MyService.class.getSimpleName();
+
     private int randomNumber;
     private boolean isRandomNumberGeneratorON;
 
     private final int MIN = 0;
     private final int MAX = 100;
+
+    Callback mCallback;
+
+    public interface Callback {
+        void getRandomNumber(int num);
+    }
+
+    public void setCallbacks(Callback callback) {
+        mCallback = callback;
+    }
 
     class MyServiceBinder extends Binder {
         public MyService getService() {
@@ -29,35 +41,39 @@ public class MyService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        Log.i("Service Demo", "In onBind");
+        Log.i(TAG, "In onBind");
         return iBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.i("Service Demo", "In onUnBind");
+        Log.i(TAG, "In onUnBind");
         return super.onUnbind(intent);
     }
 
     @Override
     public void onRebind(Intent intent) {
         super.onRebind(intent);
-        Log.i("Service Demo", "In onReBind");
+        Log.i(TAG, "In onReBind");
     }
 
     @Override
     public void onDestroy() {
         stopRandomNumberGenerator();
-        Log.i("Service Demo", "Service Destroyed");
+        Log.i(TAG, "Service Destroyed");
         super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("Service Demo", "In onStartCommand, thread id: " + Thread.currentThread().getId());
+        Log.i(TAG, "In onStartCommand, thread id: " + Thread.currentThread().getId());
 
-        int val = intent.getIntExtra("value", 0);
-        Log.i("Service Demo", "In onStartCommand, value from activity is: " + val);
+        try {
+            int val = intent.getIntExtra("value", 0);
+            Log.i(TAG, "In onStartCommand, value from activity is: " + val);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
         isRandomNumberGeneratorON = true;
         new Thread(new Runnable() {
@@ -77,19 +93,18 @@ public class MyService extends Service {
                 Thread.sleep(1000);
                 if (isRandomNumberGeneratorON) {
                     randomNumber = new Random().nextInt(MAX) + MIN;
-                    Log.i("Service Demo", "Thread id: " + Thread.currentThread().getId() + "Random Number: " + randomNumber);
+                    if (mCallback != null) {
+                        mCallback.getRandomNumber(randomNumber);
+                    }
+                    Log.i(TAG, "Thread id: " + Thread.currentThread().getId() + ", Random Number: " + randomNumber);
                 }
             } catch (InterruptedException e) {
-                Log.i("Service Demo", "Thread Interrupted");
+                Log.i(TAG, "Thread Interrupted");
             }
         }
     }
 
     private void stopRandomNumberGenerator() {
         isRandomNumberGeneratorON = false;
-    }
-
-    public int getRandomNumber() {
-        return randomNumber;
     }
 }
